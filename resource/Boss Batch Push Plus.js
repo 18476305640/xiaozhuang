a9492295f34be21893452b4cdcde090886bd0487
manZhungjie <63949859+18476305640@users.noreply.github.com>
2024-05-07 12:40:59 +0800

    Update Boss Batch Push Plus.js

--- resource/Boss Batch Push Plus.js
+++ resource/Boss Batch Push Plus.js
@@ -2,7 +2,7 @@
 // @name         Boss Batch Push Plus [Boss直聘批量投简历Plus]
 // @description  boss直聘批量简历投递
 // @namespace    maple
-// @version      1.1.9
+// @version      2.1.0
 // @author       maple,Ocyss
 // @license      Apache License 2.0
 // @run-at       document-start
